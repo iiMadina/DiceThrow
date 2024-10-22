ffc6f6da4e09cabe9ac8b7a420adccb66a88da1c
iiMadina <misrailova11@gmail.com>
2024-10-22 12:21:40 -0400

    MainActivity implements interface

--- app/src/main/java/edu/temple/dicethrow/MainActivity.kt
+++ app/src/main/java/edu/temple/dicethrow/MainActivity.kt
@@ -4,15 +4,15 @@ import android.os.Bundle
 import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), ButtonFragment.ButtonInterface {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         val fragment = DieFragment.newInstance(100)
-        findViewById<Button>(R.id.rollDiceButton).setOnClickListener {
+        /*findViewById<Button>(R.id.rollDiceButton).setOnClickListener {
             (supportFragmentManager.findFragmentById(R.id.dieContainer) as DieFragment).throwDie()
-        }
+        }*/
 
         if (supportFragmentManager.findFragmentById(R.id.dieContainer) !is DieFragment) {
         supportFragmentManager
@@ -20,5 +20,9 @@ class MainActivity : AppCompatActivity() {
             .add(R.id.dieContainer, fragment)
             .commit()
         }
+    }
+
+    override fun buttonClicked() {
+        TODO("Not yet implemented")
     }
 }
