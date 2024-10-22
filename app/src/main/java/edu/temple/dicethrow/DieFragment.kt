0a82b02ea3eb2b88fa2c59572c78c798e02bde6c
iiMadina <misrailova11@gmail.com>
2024-10-22 11:56:24 -0400

    created another bundle

--- app/src/main/java/edu/temple/dicethrow/DieFragment.kt
+++ app/src/main/java/edu/temple/dicethrow/DieFragment.kt
@@ -9,11 +9,13 @@ import android.widget.TextView
 import kotlin.random.Random
 
 const val DIESIDE = "sidenumber"
+const val KEY = "diceThrow"
 
 class DieFragment : Fragment() {
     lateinit var dieTextView: TextView
 
     var dieSides: Int = 6
+    var diceThrow : Int = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -36,14 +38,26 @@ class DieFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        throwDie()
+        savedInstanceState?.run {
+            diceThrow = getInt(KEY)
+        }
+        if (diceThrow == 0)
+            throwDie()
+        else
+            dieTextView.text = diceThrow.toString()
         /*view.setOnClickListener{
             throwDie()
         }*/
     }
 
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putInt(KEY, diceThrow)
+    }
+
     fun throwDie() {
-        dieTextView.text = (Random.nextInt(dieSides) + 1).toString()
+        diceThrow = Random.nextInt(dieSides) + 1
+        dieTextView.text = diceThrow.toString()
     }
 
     companion object {
